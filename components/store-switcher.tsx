700f09fe616cc3c9a263e030f5f2088a6c277b1b
Eiudo Gabriel <eiudogabriel@gmail.com>
2023-08-22 22:08:05 -0300

    minor fix

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -68,7 +68,7 @@ export default function StoreSwitcher({
         >
           <StoreIcon className="mr-2 h-4 w-4" />
           {currentStore?.label}
-          <ChevronsUpDown className="ml=auto h-4 w-4 shrink-0 opacity-50" />
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
